a6f3e6716fcc43aa4dbc0fd836da160d0af33926
unknown <lpc477@gmail.com>
2020-05-09 23:06:26 +0100

    Skeleton structure of components

---
+++ src/Components/BestOf.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class BestOf extends Component {
+  render(){
+    return(
+      <div>
+      This is the best of:
+      </div>
+    )
+  }
+}
+export default BestOf
